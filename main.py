cf71543c0f15d118e4a0d533b83eccc2d6e08a96
sheve <shevelev67rf@gmail.com>
2024-10-09 16:46:02 +0300

    homework 1.9

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print( 'The ilton'.istitle() )
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
+mid_index = len(example)//2
+print(example[mid_index:])
