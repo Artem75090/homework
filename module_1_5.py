cc41c442d0183e2c46bd6775add7f24a5b8e5fce
sheve <shevelev67rf@gmail.com>
2024-10-12 15:44:22 +0300

    homework 1.5

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = (7, 5.2, True, 'string')
+print(immutable_var)
+#immutable_var[1] = 5.3
+#Кортеж — это неизменяемый тип данных, который представляет собой упорядоченную последовательность элементов.
+mutable_list =[5, 28, True]
+print(mutable_list)
+mutable_list[2] = False
+print(mutable_list)
