57377e39c088f72c2ffa1118ceea7e5da7423529
sheve <shevelev67rf@gmail.com>
2024-11-11 15:59:41 +0300

    homework 3(2)

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,19 +1,21 @@
-list_data = []
+
 def sum_data(*args):
+    summa = 0
 
     while args:
         if type(args) == tuple:
             args = list(args)
         a = args.pop()
         if isinstance(a, (int, float)):
-            list_data.append(a)
+            summa += a
         if type(a) == str:
-            list_data.append(len(a))
+            summa += (len(a))
         if isinstance(a, (list, tuple, set)):
-            sum_data(*a)
+            summa += sum_data(*a)
         if type(a) == dict:
-            sum_data(*(list(a.values()) + list(a.keys())))
-    return (sum(list_data))
+            summa += sum_data(*(list(a.values()) + list(a.keys())))
+    return summa
 
 
 print(sum_data([[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]))
+
