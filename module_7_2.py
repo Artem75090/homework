2e69b28f57c04f26d054b2fa01a6510552e15852
sheve <shevelev67rf@gmail.com>
2024-12-03 14:55:43 +0300

    homework 7_2(2)

--- module_7_2.py
+++ module_7_2.py
@@ -13,33 +13,7 @@ def custom_write(file_name, strings):
     return strings_positions
     file.close
 
-
-
-
-
-
-
-
-
-
-
-
-info = [
-
-    'Text for tell.',
-
-    'Используйте кодировку utf-8.',
-
-    'Because there are 2 languages!',
-
-    'Спасибо!'
-
-    ]
-
-
-
+info = ['Text for tell.', 'Используйте кодировку utf-8.', 'Because there are 2 languages!', 'Спасибо!']
 result = custom_write('test.txt', info)
-
 for elem in result.items():
-
-  print(elem)
+    print(elem)
