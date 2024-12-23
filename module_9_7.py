6ea5a100fadb3a7d80e05ac2ea1a05f59fb6987a
sheve <shevelev67rf@gmail.com>
2024-12-23 15:26:12 +0300

    homework 9_7(2)

--- module_9_7.py
+++ module_9_7.py
@@ -1,17 +1,16 @@
 def is_prime(func):
     def wrapper(a, b, c):
-        is_prime = True
+        flag = True
         res_sum_three = func(a, b, c)
         for i in range(2, res_sum_three):
             if res_sum_three % i == 0:
-                is_prime = False
+                flag = False
                 break
-        if is_prime:
+        if flag:
             print(f"Простое")
-            return res_sum_three
         else:
             print(f"Составное")
-            return res_sum_three
+        return res_sum_three
 
     return wrapper
 
